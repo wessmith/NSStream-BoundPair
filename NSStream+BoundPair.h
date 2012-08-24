0acd7502b80c5729d824e6915f2fd9c3de4d3e2e
Wes Smith <virtualaudio@gmail.com>
2012-08-24 15:03:36 -0400

    Removed legacy code. Now only iOS 5 and OS X 10.7 support.

--- NSStream+BoundPair.h
+++ NSStream+BoundPair.h
@@ -1,6 +1,8 @@
 //
 //  NSStream+BoundPair.h
 //
+//  Modified by Wes Smith: 8-24-12
+//
 //  Extracted From: SimpleURLConnections sample project WWDC 2012
 //  Written by: DTS
 //  Copyright:  Copyright (c) 2009-2012 Apple Inc. All Rights Reserved.
@@ -11,7 +13,7 @@
 // A category on NSStream that provides a nice, Objective-C friendly way to create
 // bound pairs of streams.
 
-@interface NSStream (BoundPairAdditions)
+@interface NSStream (BoundPair)
 
 + (void)createBoundInputStream:(NSInputStream **)inputStreamPtr outputStream:(NSOutputStream **)outputStreamPtr bufferSize:(NSUInteger)bufferSize;
 
